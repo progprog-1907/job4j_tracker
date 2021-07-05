c0293d36dc24a9b253cb28faaf2d2167793de1ec
progprog-1907 <progprog1907@gmail.com>
2021-07-05 15:43:57 +0400

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {}
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Online: " + active);
+        System.out.println("Status number: " + status);
+        System.out.println("Message " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.active = false;
+        error.status = 2;
+        error.message = "Bye";
+        error.printInfo();
+
+        Error truth = new Error(true, 1, "Hello");
+        truth.printInfo();
+    }
+}
