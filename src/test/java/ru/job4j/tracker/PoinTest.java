da73745c50bb4a09d92c0f53f563bd68dcbe7edc
progprog-1907 <progprog1907@gmail.com>
2021-07-06 10:29:10 +0400

    2.1. Рефакторинг - Расстояние между точками. [#122587]

---
+++ src/test/java/ru/job4j/tracker/PoinTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.tracker;
+
+import org.junit.Assert;
+import org.junit.Test;
+import ru.job4j.oop.Point;
+
+public class PoinTest {
+    @Test
+    public void when00to20then2() {
+        Point x = new Point(0, 0);
+        Point y = new Point(2, 0);
+        int expected = 2;
+        double out = x.distance(y);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when10to40then3() {
+        Point x = new Point(1, 0);
+        Point y = new Point(4, 0);
+        int expected = 3;
+        double out = x.distance(y);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
