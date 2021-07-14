6a6f4ee169d3673215035520da2168bea9517319
progprog-1907 <progprog1907@gmail.com>
2021-07-14 12:01:30 +0400

    4. Переопределение [#174890 #177479]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,9 +1,13 @@
 package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport{
-    final char dm = (char) 34;
+    public JSONReport() {
+
+    }
+
     @Override
     public String generate(String name, String body) {
+        char dm = (char) 34;
         return "{" + System.lineSeparator() + dm + "name" + dm + " : " + dm + name + dm + "," +
                 System.lineSeparator() + dm + "body" + dm + " : " + dm + body + dm +
                 System.lineSeparator() + "}";
