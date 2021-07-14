83b11c78d0c15c4b2ae3eb1700b453843f1e3b0b
progprog-1907 <progprog1907@gmail.com>
2021-07-14 11:04:53 +0400

    5. Аннотация @Override [#174891]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -2,6 +2,7 @@ package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport{
     final char dm = (char) 34;
+    @Override
     public String generate(String name, String body) {
         return "{" + System.lineSeparator() + dm + "name" + dm + " : " + dm + name + dm + "," +
                 System.lineSeparator() + dm + "body" + dm + " : " + dm + body + dm +
