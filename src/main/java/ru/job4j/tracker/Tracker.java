5477e39856beb617570a336f3f7c8effa91c5b26
progprog-1907 <progprog1907@gmail.com>
2021-07-23 12:55:15 +0400

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -13,16 +13,20 @@ public class Tracker {
         return item;
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
         return rsl;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findByName(String key) {
@@ -40,5 +44,13 @@ public class Tracker {
 
     public Item[] findAll() {
         return Arrays.copyOf(items, size);
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        int oldId = items[index].getId();
+        item.setId(oldId);
+        items[index] = item;
+        return item.getId() == id;
     }
 }
