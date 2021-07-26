0a7973a974aa90e44c18865e84c46b99bffda1a8
progprog-1907 <progprog1907@gmail.com>
2021-07-26 09:41:56 +0400

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/DropArray.java
+++ src/main/java/ru/job4j/tracker/DropArray.java
@@ -4,18 +4,10 @@ import java.util.Arrays;
 
 public class DropArray {
     public static void main(String[] args) {
-        String[] names = {"Petr", null, "Ivan", "Stepan", null};
-        String[] namesWithoutNull = new String[names.length];
-        int size = 0;
-        for (String name : names) {
-            if (name != null) {
-                namesWithoutNull[size] = name;
-                size++;
-            }
-        }
-        namesWithoutNull = Arrays.copyOf(namesWithoutNull, size);
-        for (String s : namesWithoutNull) {
-            System.out.println(s);
-        }
+        String[] names = {"Petr", null, "Ivan", "Stepan", "Fedor"};
+        System.arraycopy(names, 2, names, 1, 3);
+        System.out.println(Arrays.toString(names));
+        names[names.length - 1] = null;
+        System.out.println(Arrays.toString(names));
     }
 }
