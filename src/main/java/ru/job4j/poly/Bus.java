8fed9a1cf7742ca84ad1473a9c4dbbfa9e084642
progprog-1907 <progprog1907@gmail.com>
2021-08-06 11:39:32 +0400

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+    @Override
+    public void go() {
+
+    }
+
+    @Override
+    public void passengers(int countOfPassengers) {
+
+    }
+
+    @Override
+    public int refuel(int amountOfFuel) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,7 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    void go();
+    void passengers(int countOfPassengers);
+    int refuel(int amountOfFuel);
+}
