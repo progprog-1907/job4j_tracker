46c9e6d3cc1ee901e78d581353b09d58601c1ad1
progprog-1907 <progprog1907@gmail.com>
2021-08-10 14:57:27 +0400

    5.1. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUiTest.java
+++ src/test/java/ru/job4j/tracker/StartUiTest.java
@@ -26,4 +26,46 @@ public class StartUiTest {
         Item expected = new Item("Iman");
         assertThat(created.getName(), is(expected.getName()));
     }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()), /* id сохраненной заявки в объект tracker. */
+                "replaced item"
+        };
+        StartUI.replaceItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
+    }
+
+    @Test
+    public void whenReplaceItem1() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("item");
+        tracker.add(item);
+        String[] answer = {
+                String.valueOf(item.getId()),
+                "replaced"
+        };
+        StartUI.replaceItem(new StubInput(answer), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("item");
+        tracker.add(item);
+        String[] answer = {
+                String.valueOf(item.getId()),
+                "deleted item"
+        };
+        StartUI.deleteItem(new StubInput(answer), tracker);
+        Item deleted = tracker.findById(item.getId());
+        assertThat(deleted.getName(), is("null"));
+    }
 }
