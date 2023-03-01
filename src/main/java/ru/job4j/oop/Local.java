e3abb9b0199c4398aff182580e41a41a0ae7d8da
iman.xai93 <iman.xai93@gmail.com>
2023-03-01 22:18:20 +0400

    6.2. Локальные и анонимные внутренние классы [#504952 #396581]

---
+++ src/main/java/ru/job4j/oop/Local.java
@@ -0,0 +1,25 @@
+package ru.job4j.oop;
+
+public class Local {
+    private String name = "Petr";
+
+    public void getFullName() {
+        final String surname = "Arsentev";
+
+        class FullName {
+            public static final String PATRONYMIC = "Empty";
+            public void printFullName() {
+                System.out.println(name + " " + surname);
+                System.out.println(PATRONYMIC);
+            }
+        }
+
+        FullName fullName = new FullName();
+        fullName.printFullName();
+    }
+
+    public static void main(String[] args) {
+        Local local = new Local();
+        local.getFullName();
+    }
+}
