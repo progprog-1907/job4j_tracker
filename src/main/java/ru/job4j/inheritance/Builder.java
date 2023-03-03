9621b31e7ba6e94dd7c93d157d069b315539ef7e
iman.xai93 <iman.xai93@gmail.com>
2023-03-03 14:20:42 +0400

    3. Вызов конструктора родителя super(...) [#6837 #176691]

--- src/main/java/ru/job4j/inheritance/Builder.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.inheritance;
-
-public class Builder extends Engineer {
-    public Builder(String name, String surname, String education, String birthday) {
-        super(name, surname, education, birthday);
-        super.buildHouse();
-    }
-}
--- src/main/java/ru/job4j/inheritance/Dentist.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.inheritance;
-
-public class Dentist extends Doctor {
-    public Dentist(String name, String surname, String education, String birthday) {
-        super(name, surname, education, birthday);
-        super.dentures();
-    }
-}
