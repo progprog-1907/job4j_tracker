7184662d1749a9d5a6b36d96f7ad86fbe5315a32
iman.xai93 <iman.xai93@gmail.com>
2023-03-29 12:43:56 +0400

    12.0. Написать тесты на StartUI. [#785 #404680]

--- src/main/java/ru/job4j/tracker/ReplaceAction.java
+++ src/main/java/ru/job4j/tracker/ReplaceAction.java
@@ -13,7 +13,6 @@ public class ReplaceAction implements UserAction {
         int id = input.askInt("Enter id: ");
         String name = input.askStr("Enter name: ");
         Item item = new Item(name);
-        tracker.replace(id, item);
         if (tracker.replace(id, item)) {
             System.out.println("Заявка изменена успешно.");
         } else {
