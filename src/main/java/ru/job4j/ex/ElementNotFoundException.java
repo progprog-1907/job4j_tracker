804c769bd39435bc64fbf274d918fc6b3b11b6c9
iman.xai93 <iman.xai93@gmail.com>
2023-04-04 11:36:48 +0400

    0.4. Пользовательские исключения. [#219367]

--- src/main/java/ru/job4j/ex/ElementNotFoundException.java
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -1,7 +1,5 @@
 package ru.job4j.ex;
 
 public class ElementNotFoundException extends Exception {
-    public ElementNotFoundException(String massage) {
-        super(massage);
-    }
+
 }
--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,29 +1,5 @@
 package ru.job4j.ex;
 
 public class FindEl {
-    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-        int rsl = -1;
-        for (int i = 0; i < value.length; i++) {
-            if (key.equals(value[i])) {
-                rsl = i;
-                break;
-            }
-        }
 
-        if (rsl == -1) {
-            throw new ElementNotFoundException("Такого элемента нет в массиве!");
-        }
-
-        return rsl;
-    }
-
-    public static void main(String[] args) {
-        String[] value = new String[]{"Halo", "Hello", "Privet", "Salam"};
-
-        try {
-            indexOf(value, "Privit");
-        } catch (ElementNotFoundException e){
-            e.printStackTrace();
-        }
-    }
 }
