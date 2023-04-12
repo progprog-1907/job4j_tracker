c256a4362004e87ce88f8d8b2481720ed755b8e6
iman.xai93 <iman.xai93@gmail.com>
2023-04-12 08:54:07 +0400

    0.3. Тестирование исключений с junit [#219366 #405837]

--- src/test/java/ru/job4j/ex/CountTest.java
+++ src/test/java/ru/job4j/ex/CountTest.java
@@ -5,12 +5,5 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CountTest {
-    @Test
-    public void whenException() {
-        int start = 0;
-        int finish = 3;
-        int expected = 3;
-        int result = Count.add(start, finish);
-        assertThat(result).isEqualTo(expected);
-    }
+
 }
