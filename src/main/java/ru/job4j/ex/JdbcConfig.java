9adfb8505ae2577857de6adefcc459bddb6df107
iman.xai93 <iman.xai93@gmail.com>
2023-04-12 15:34:30 +0400

    0.5. Иерархия исключений и множественный catch. [#219368]

--- src/main/java/ru/job4j/ex/JdbcConfig.java
+++ src/main/java/ru/job4j/ex/JdbcConfig.java
@@ -5,7 +5,6 @@ public class JdbcConfig {
         if (url == null) {
             throw new UserInputException("Url could not be null");
         }
-        /*load jdbc*/
     }
 
     public static void main(String[] args) {
