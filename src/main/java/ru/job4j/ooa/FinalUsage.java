3813fa78969d1ba0c63932390054c517c2fd4d18
iman.xai93 <iman.xai93@gmail.com>
2023-04-27 02:00:31 +0400

    3. final [#334073]

---
+++ src/main/java/ru/job4j/ooa/FinalUsage.java
@@ -0,0 +1,11 @@
+package ru.job4j.ooa;
+
+public class FinalUsage {
+    private static final String NAME = "Petr";
+    private static final int DEFAULT_STORE_CAPACITY = 100;
+
+    public static void main(String[] args) {
+        System.out.println(NAME);
+        System.out.println(DEFAULT_STORE_CAPACITY);
+    }
+}
